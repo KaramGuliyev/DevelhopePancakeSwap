d40e88f9e7995ad3be734973a096facaec7a462e
Karam Guliyev <karamguliyev10@gmail.com>
2023-01-31 15:50:44 +0400

    scrollToTop Button fixed

--- scripts/scrollToTop.js
+++ scripts/scrollToTop.js
@@ -3,10 +3,22 @@ let scrollToTop = document.querySelector("#scrollToTop");
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
   if (currentScroll > window.innerHeight) {
-    scrollToTop.classList.add("visible");
-    scrollToTop.classList.remove("hidden");
-  } else if(scrollToTop.classList.contains("visible")) {
-    scrollToTop.classList.remove("visible");
-    scrollToTop.classList.add("hidden");
+    scrollToTop.setAttribute("class", "scrollToTop visible");
+    scrollToTop.setAttribute("style", "")
+
+  } else if (scrollToTop.classList.contains("visible")) {
+    scrollToTop.setAttribute("class", "scrollToTop hidden");
+    setTimeout(() => {
+      scrollToTop.setAttribute("style", "display:none;");
+    }, 500);
   }
 });
+
+let scrollToTopButton = document.querySelector(".scrollButton");
+scrollToTopButton.addEventListener("click", () => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+

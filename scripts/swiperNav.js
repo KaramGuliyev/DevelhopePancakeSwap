6c42ed86a05d2329c446bbb4dc80e9745b48b7f5
Karam Guliyev <karamguliyev10@gmail.com>
2023-02-23 23:07:00 +0300

    Carousel NavButtons Finished

--- scripts/swiperNav.js
+++ scripts/swiperNav.js
@@ -1,17 +1,29 @@
 let carouselBtn = document.getElementsByClassName('carouselNavButton')
-let carouselWrapper = document.getElementsByClassName('carouselWrapper')
-
-console.log(carouselWrapper)
+let sliders = document.getElementsByClassName('slider')
 
 for (let i = 0; i < carouselBtn.length; i++) {
   const element = carouselBtn[i]
-  carouselBtn[i].addEventListener('click', (e) => {
-    handleChangeCarousel(e.target)
+  element.addEventListener('click', (e) => {
+    handleChange(e.target.id, sliders)
   })
 }
 
-const handleChangeCarousel = (e) => {
-  console.log(e.id)
-  console.log(carouselWrapper)
-  console.log(carouselWrapper[e.id])
+const handleChange = (e) => {
+  console.log(e)
+  if (carouselBtn[e - 1].classList.contains('active')) {
+  } else if (e === '1' && !carouselBtn[e - 1].classList.contains('active')) {
+    setNewActiveButton(e)
+  } else if (!sliders[e - 1].classList.contains('active')) {
+    setNewActiveButton(e)
+  }
+}
+
+const setNewActiveButton = (e) => {
+  for (let i = 0; i < carouselBtn.length; i++) {
+    const element = carouselBtn[i]
+    element.classList.remove('active')
+  }
+  setTimeout(() => {
+    carouselBtn[e - 1].classList.add('active')
+  }, 10)
 }
